250217b04c9e79c25a433c648758de6b5a555992
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-04-26 00:03:43 +0300

    refactor Cards.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,5 @@
 function Card({card, id}) {
   return (
-  //   card.map(item => {
-  //     return (
-  //       <li className="card__item" key={item._id}>
-  //         <button className="card__trash hover" type="button" aria-label="удалить карточку"></button>
-  //         <img className="card__img" src={item.link} alt={item.name} />
-  //         <div className="card__footer">
-  //           <h2 className="card__text">{item.name}</h2>
-  //           <div className="card__like-wraper">
-  //             <button className="card__like" type="button" aria-label="лайк"></button>
-  //             <span className="card__counter">{item.likes.length}</span>
-  //           </div>
-  //         </div>
-  //       </li>
-  //     )
-  //   })
-
     <li className="card__item">
       <button className="card__trash hover" type="button" aria-label="удалить карточку"></button>
       <img className="card__img" src={card.link} alt={card.name} />
