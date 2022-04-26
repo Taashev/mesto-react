8c0a534c82fd42c2e4985433c6999c92cfacdc05
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-04-26 03:21:35 +0300

    add img fullscreen alt

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -3,7 +3,7 @@ function ImagePopup({ card, onClose }) {
     <div className={`popup popup_type_fullscreen ${card ? 'popup_opened' : ''}`}>
       <div className="popup__container-fullscreen">
         <button className="popup__close hover" type="button" aria-label="закрыть всплывающее окно" onClick={onClose}></button>
-        <img className="popup__full-img" src={card.link} alt="#" />
+        <img className="popup__full-img" src={card.link} alt={card.name} />
         <h2 className="popup__full-text">{card.name}</h2>
       </div>
     </div>
